28b73991c16308f29ab2e4cf2dfa482ae56b98a1
Emanuel Gelvez <81266779+JoelS711@users.noreply.github.com>
2024-06-26 21:28:33 -0500

    Se agrega ejercicio 13

---
+++ exp-013.py
@@ -0,0 +1,13 @@
+MiDiccionario = {
+'Nombre' : 'Margarita',
+'Edad' : 55,
+'Genero' : 'Femenino'
+}
+ 
+print(MiDiccionario['Nombre'])
+
+matrix = [[1,2][3,4]]
+print(matrix[1][0])
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        print(matrix[i][j])
